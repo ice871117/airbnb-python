e1ca6583108b87688ba8032f32c3016418713dfa
wiizhang <414191673@qq.com>
2019-05-31 01:32:36 +0800

    添加部分搜索

--- StorageService.py
+++ StorageService.py
@@ -12,8 +12,11 @@ class StorageService:
 
     def __init__(self, path):
         dbPath = path if path else self._DEFAULT_PATH
-        self.prepareDB(dbPath)
+        self.storage = self.prepareDB(dbPath)
         pass
 
     def prepareDB(self, path):
-        print()
+        conn = sqlite3.connect(path)
+        cursor = conn.cursor()
+        cursor.execute("create table if not exist user (id varchar(20) primary key, name varchar(20))")
+        return cursor
